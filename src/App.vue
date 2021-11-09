f4d0ce960ff67ef8560bc3d02b2fc4303308c8ba
Josh Cornau <hi@joshcornau.de>
2021-11-09 10:25:17 +0100

    Dynamicaly Load Comment Files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <DisplayProblems
+      v-if="this.Data[1].interestingcomments"
+      v-bind:Heading="'Problem'"
+      v-bind:Data="this.Data[1].interestingcomments"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DisplayProblems from "./components/DisplayProblems.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    DisplayProblems,
+  },
+  data: function () {
+    return {
+      Data: [],
+      json: null, // passing array data into Vue
+    };
+  },
+  async created() {
+    const foobar = await this.getJson();
+    foobar.forEach(async (item) => {
+      let temp = await import("./json/" + item.replace("./", ""));
+      this.Data.push(temp);
+      console.log("test", this.Data[0].interestingcomments);
+    });
+  },
+  methods: {
+    getJson() {
+      const files = require.context("@/json", true, /^.*json$/);
+      files.keys();
+      console.log(files.keys());
+      return files.keys();
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/DisplayProblems.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="hello">
+    <h1>{{this.Heading}}</h1>
+     <ul class="grid">
+    <li v-for="item in this.Data" :key="item.name" class="item">
+      <div class="heading">
+        <span class="autor">{{ item.autor+" "}}  </span>
+      <span class="date">{{ item.date }}</span>
+      </div>
+      <br>
+      <span v-html="SplitProblems(item.content) "></span>
+    </li>
+  </ul>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DisplayProblems',
+  props: {
+    Data: JSON,
+    Heading: String
+  },
+  methods: {
+    SplitProblems(string){
+      let checked = string.split(/\s+/);
+      let newChecked = []
+      checked.forEach(function(item){
+            if(item=='----->'){
+                newChecked.push(item.replace('----->','<span style="color:blue; background-color: rgba(172, 209, 244, 0.2); font-weight: bold; border-radius: 3px; width: auto;  height: auto; padding: 2px 2px 2px 2px; margin-left:2px;margin-right:2px">'));
+            }else if (item=='!!!'){
+                newChecked.push(item.replace('!!!','</span>'));
+            }else{
+              newChecked.push(item)
+            }})
+      
+      return newChecked.join(" ")
+  
+      
+       
+        
+      }
+
+  
+    } 
+    }
+
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.grid{
+  column-count: 2;
+  column-gap: 20;
+  padding: 10px;
+  text-align: left;
+}
+.autor{
+  font-weight: bold;
+}
+.date{
+  font-style:italic;
+  color:blue
+}
+
+ 
+
+.item{
+    
+  display: inline-block; /* important to wrap notes not content */
+  width: 98%;
+  background-color:#FFFFFF;
+  margin-top: 20px;
+  padding: 1%;
+  border-radius: 5px;
+  box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+}
+
+
+/* .item{
+    margin: 1%;
+  width:45%;
+ display:block;
+  float:left;
+  box-sizing: border-box;
+  padding: 1%;
+  background-color:#FFFFFF;
+  border-radius: 5px;
+  box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+}
+.item:nth-child(3n + 0) { float: right; } */
+
+.red{
+  color: red;
+}
+h3 {
+  margin: 40px 0 0;
+}
+h1 {
+text-align: left;
+ margin: 1%;
+ font-size: 50px;
+
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
