b4777a998a72351c8aaa6712187dfc084587ea53
Josh Cornau <hi@joshcornau.de>
2021-11-10 09:58:52 +0100

    Using Files from .json Folder

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
-    <DisplayProblems
-      v-if="this.Data[1].interestingcomments"
-      v-bind:Heading="'Problem'"
-      v-bind:Data="this.Data[1].interestingcomments"
-    />
+    <div class="switch">
+      <div
+        v-for="(item, index) in this.Data"
+        :key="item.name"
+        class="list-item"
+      >
+        <a
+          v-on:click="counter = index"
+          v-bind:class="[{ listItemSelected: counter === index }]"
+        >
+          <h1>{{ this.Data[index].name }}</h1>
+        </a>
+      </div>
+    </div>
+
+    <div>
+      <DisplayProblems
+        v-if="this.Data[counter].interestingcomments"
+        v-bind:Heading="'Problem'"
+        v-bind:Data="this.Data[counter].interestingcomments"
+      />
+    </div>
   </div>
 </template>
 
@@ -18,6 +35,8 @@ export default {
   },
   data: function () {
     return {
+      counter: 0,
+      test: true,
       Data: [],
       json: null, // passing array data into Vue
     };
@@ -27,7 +46,7 @@ export default {
     foobar.forEach(async (item) => {
       let temp = await import("./json/" + item.replace("./", ""));
       this.Data.push(temp);
-      console.log("test", this.Data[0].interestingcomments);
+      console.log("test", this.Data);
     });
   },
   methods: {
@@ -42,6 +61,20 @@ export default {
 </script>
 
 <style>
+.switch {
+  display: flex;
+}
+.list-item {
+  margin: 1%;
+  margin-right: 30px;
+  cursor: pointer;
+}
+
+.listItemSelected {
+  color: blue;
+  text-decoration: underline blue;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -49,5 +82,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
--- src/components/DisplayProblems.vue
+++ src/components/DisplayProblems.vue
@@ -1,86 +1,78 @@
 <template>
   <div class="hello">
-    <h1>{{this.Heading}}</h1>
-     <ul class="grid">
-    <li v-for="item in this.Data" :key="item.name" class="item">
-      <div class="heading">
-        <span class="autor">{{ item.autor+" "}}  </span>
-      <span class="date">{{ item.date }}</span>
-      </div>
-      <br>
-      <span v-html="SplitProblems(item.content) "></span>
-    </li>
-  </ul>
-
+    {{ this.Data.length }}
+    <ul class="grid">
+      <li v-for="item in this.Data" :key="item.name" class="item">
+        <div class="heading">
+          <span class="autor">{{ item.autor + " " }} </span>
+          <span class="date">{{ item.date }}</span>
+        </div>
+        <br />
+        <span v-html="SplitProblems(item.content)"></span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'DisplayProblems',
+  name: "DisplayProblems",
   props: {
     Data: JSON,
-    Heading: String
+    Heading: String,
   },
   methods: {
-    SplitProblems(string){
+    SplitProblems(string) {
       let checked = string.split(/\s+/);
-      let newChecked = []
-      checked.forEach(function(item){
-            if(item=='----->'){
-                newChecked.push(item.replace('----->','<span style="color:blue; background-color: rgba(172, 209, 244, 0.2); font-weight: bold; border-radius: 3px; width: auto;  height: auto; padding: 2px 2px 2px 2px; margin-left:2px;margin-right:2px">'));
-            }else if (item=='!!!'){
-                newChecked.push(item.replace('!!!','</span>'));
-            }else{
-              newChecked.push(item)
-            }})
-      
-      return newChecked.join(" ")
-  
-      
-       
-        
-      }
 
-  
-    } 
-    }
+      let newChecked = [];
 
+      checked.forEach(function (item) {
+        if (item == "----->") {
+          newChecked.push(
+            item.replace(
+              "----->",
+              '<span style="color:blue; background-color: rgba(172, 209, 244, 0.2); font-weight: bold; border-radius: 3px; width: auto;  height: auto; padding: 2px 2px 2px 2px; margin-left:2px;margin-right:2px">'
+            )
+          );
+        } else if (item == "!!!") {
+          newChecked.push(item.replace("!!!", "</span>"));
+        } else {
+          newChecked.push(item);
+        }
+      });
 
+      return newChecked.join(" ");
+    },
+  },
+};
 </script>
-
-
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.grid{
+.grid {
   column-count: 2;
   column-gap: 20;
   padding: 10px;
   text-align: left;
 }
-.autor{
+.autor {
   font-weight: bold;
 }
-.date{
-  font-style:italic;
-  color:blue
+.date {
+  font-style: italic;
+  color: blue;
 }
 
- 
-
-.item{
-    
+.item {
   display: inline-block; /* important to wrap notes not content */
   width: 98%;
-  background-color:#FFFFFF;
+  background-color: #ffffff;
   margin-top: 20px;
   padding: 1%;
   border-radius: 5px;
   box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
 }
-
 
 /* .item{
     margin: 1%;
@@ -95,17 +87,16 @@ export default {
 }
 .item:nth-child(3n + 0) { float: right; } */
 
-.red{
+.red {
   color: red;
 }
 h3 {
   margin: 40px 0 0;
 }
 h1 {
-text-align: left;
- margin: 1%;
- font-size: 50px;
-
+  text-align: left;
+  margin: 1%;
+  font-size: 50px;
 }
 ul {
   list-style-type: none;
